e12de9b2b6b949ee88d55fd5ffbc7010756aa630
xWased <35819426+xWased@users.noreply.github.com>
2018-01-25 22:03:50 +0000

    Update Main.php
    #FIX NO GIVEN EFFECT

--- src/Muqsit/Main.php
+++ src/Muqsit/Main.php
@@ -46,7 +46,10 @@ class Main extends PluginBase implements Listener{
 	 * @param int $amplifier
 	 */
 	public static function giveEffect(Player $player, $id, $duration, $amplifier){
-		$effect = Effect::getEffect($id)->setDuration($duration)->setAmplifier($amplifier); # Fluent setters <3
+		$effect = Effect::getEffect($id); # Fluent setters <3
+		$effect->setDuration($duration);
+                $effect->setAmplifier($amplifier);
+	
 		$player->addEffect($effect);
 	}
 }
